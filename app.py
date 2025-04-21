13c10587789da026ed725687b50ff999debe4713
Gopal Mahato <gopalmahato0701@gmail.com>
2025-04-21 14:20:17 +0530

    initial SkySwift deployment

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template('how_it_works.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/coming-soon')
+def coming_soon():
+    return render_template('coming_soon.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
